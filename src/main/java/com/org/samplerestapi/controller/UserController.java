b5ea90b3ba689a85a224c480d7bbf48f32f0b064
sfonseka <sfonseka@wiley.com>
2023-01-20 17:15:55 +0530

    Create new API for login user

---
+++ src/main/java/com/org/samplerestapi/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.org.samplerestapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "api/user")
+public class UserController {
+    @PostMapping("/login")
+    public ResponseEntity<String> logUser(@RequestBody Map<String,String> user ){
+        var username = user.get("username");
+        var password = user.get("password");
+
+        if(username.equals("admin") && password.equals("admin")) {
+            return new ResponseEntity<>("OK", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+    }
+}
+
